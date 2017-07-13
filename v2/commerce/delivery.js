8613ed8c5e6996e988fe706dfb32c5d01002adc6
risingashes <floatingsurfboard@gmail.com>
2017-07-13 10:09:22 +1000

    Update delivery.js

--- v2/commerce/delivery.js
+++ v2/commerce/delivery.js
@@ -7,22 +7,42 @@
 // entries or omitted entries, but they should clear up within
 // five minutes. Cache coherency is a hard problem.
 
-// GET /v2/commerce/delivery?access_token=<account+tradingpost>
+
+// NOTE: These endpoints are paginated and accept the
+// ?page query parameter. Each page evaluates 500,000 individual 
+// transactions and concatanates the results based upon item id.
+// Only outliers will require more than one page of results.
+
+// GET /v2/commerce/delivery?access_token=<account+tradingpost>&page=0
 
 {
+  //First 500,000 results. Concatanated by item id
   "coins": 4294967295,
   "items": [
     {
-      "id": 19700,
-      "count": 3
+      "id": 19701,
+      "count": 500 //162 stacks found, 500 items total
     },
     {
       "id": 19700,
-      "count": 4
+      "count": 1249592 //499,837 stacks found, 1249592 items total
     },
     {
       "id": 25557,
       "count": 1
     }
   ]
+}
+
+// GET /v2/commerce/delivery?access_token=<account+tradingpost>&page=1
+{
+	//Results 500,001 to 1,000,000. Concatanated by item id.
+}
+
+// NOTE: Not including the ?page query parameter for an account with
+// over 500,000 transactions will result in an error.
+
+// GET /v2/commerce/delivery?access_token=<account+tradingpost>
+{
+  
 }
